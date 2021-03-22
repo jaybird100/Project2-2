f296776d295b24df7d308405b5b197a9c6ddcb71
A-gamil <alaagamil5264@gmail.com>
2021-03-22 21:30:55 +0300

    fixed timer

--- src/Actions/Set.java
+++ src/Actions/Set.java
@@ -3,6 +3,7 @@ package Actions;
 import Articles.Article;
 import Articles.Timer;
 import Utils.Variables;
+import javafx.animation.AnimationTimer;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
@@ -10,6 +11,7 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
 
 public class Set extends Action{
     Article type;
@@ -26,8 +28,10 @@ public class Set extends Action{
             Stage stage = new Stage();
             stage.setTitle("Timer");
             GridPane root= new GridPane();
-            LocalTime a= LocalTime.of(t.hr,t.min,t.sec);
-            Label timeInterval= new Label("Time remaining: "+a.toString());
+            final LocalTime[] a = {LocalTime.of(t.hr, t.min, t.sec)};
+
+
+            Label timeInterval= new Label("Time remaining: "+ a[0].toString());
             Label timeNow=new Label("Time at start: "+LocalTime.now().toString());
             Label endTime =new Label("Time at end: " +t.tf.toString());
 
@@ -35,21 +39,35 @@ public class Set extends Action{
             root.add(timeInterval,0,0);
             root.add(timeNow, 0 ,1);
             root.add(endTime,0,2);
+            final long[] lastTime = {System.nanoTime()};
 
+            AnimationTimer timer = new AnimationTimer() {
+                @Override
+                public void handle(long now) {
+                    if(a[0].equals(LocalTime.of(0,0,0))){
+                        this.stop();
+                        System.out.println("done");
+                        stage.close();
+                    }else{
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        timeInterval.setText("Time remaining: " + a[0].minusSeconds(1));
+                        a[0] = a[0].minusSeconds(1);
+                    }
+                }
 
-            stage.setScene(new Scene(root, Variables.SCREEN_WIDTH/10, Variables.SCREEN_HEIGHT/10));
+            };
+            timer.start();
+            stage.setScene(new Scene(root, Variables.SCREEN_WIDTH/5, Variables.SCREEN_HEIGHT/10));
             stage.show();
-//            while(t.tf.getTime().isAfter(LocalTime.now())) {
-//                if (ti == 1000) {
-//                    timeInterval.setText("Time remaining: " + a.minusSeconds(1));
-//                } else {
-//                    ti = LocalTime.now().getSecond() + ti;
-//                }
-//
-//            }
+
+
         }
 
-        return "reached outside";
+        return "Timer started";
     }
 
     public String toString(){return "Set";}
