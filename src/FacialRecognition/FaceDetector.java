94d7f1a54df5069c13efb2ae285362e1b6bb607d
EmeryK-1 <56229024+EmeryK-1@users.noreply.github.com>
2021-06-10 19:39:27 +0200

    Removed opencv stuff
    Pain in the ass to download

--- src/FacialRecognition/FaceDetector.java
+++
@@ -1,9 +0,0 @@
-package FacialRecognition;
-
-import javafx.stage.Stage;
-
-public interface FaceDetector {
-    public void init(Stage stage);
-    public void findFace();
-    public boolean isFaceFound();
-}
